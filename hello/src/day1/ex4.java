102570dec5661cee9d4d752d72c07ccbc12b200e
younho <sino012ho@naver.com>
2019-03-29 14:11:09 +0900

    1일차 자바

---
+++ hello/src/day1/ex4.java
@@ -0,0 +1,29 @@
+package day1;
+
+import java.util.Scanner;
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*String name = "ja" + "ba";
+				  
+		String str = "name" + 8.0;
+				 
+	System.out.println(name);
+	System.out.println(str);
+	System.out.println(7+" ");
+	System.out.println(" "+7);
+	System.out.println(7+ " ");
+	*/
+		
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		int a = Integer.parseInt(input);
+		System.out.println(a);
+	
+		
+	}
+
+}
