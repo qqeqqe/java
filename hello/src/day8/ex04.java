feb7cd9d9a482c67b8580b464897805648ba9dce
younho <sino012ho@naver.com>
2019-04-04 16:56:12 +0900

    자바 8일차

---
+++ hello/src/day8/ex04.java
@@ -0,0 +1,36 @@
+package day8;
+
+import java.util.Scanner;
+
+public class ex04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	Scanner scan = new Scanner(System.in);
+		int a = 0;
+		a = scan.nextInt();*/
+		
+		/*메소드에서 리턴값이 있을때 구현시 유의사항
+		조건문을 통해 리턴하는 경우나 반복문을 통해 리턴할 경우 리턴되지 않는 경우을 항상 생각해야 한다
+		반복문 안에서 return을 할 경우 조심해야 한다
+		조건문을 통해 return할 경우 조건문이 거짓인 경우를 고려해야한다
+		반복문 안에서 return을 할 경우 반복문 조건이 거짓이 되어 반복문이 실행되지 않을 경우를 고려해야한다*/
+		
+		
+	}
+		public static int test1(){
+			int r = 0;
+			for(int i=1; i<=9; i++){
+				r += i; 
+			return r;
+			}
+			/*반복문이 무조건 실행되서 리턴이 되는 겨우라 하더라도 자바 컴파일러는
+			세세한 내용까지 검사하지 않기 때문에 반복문이 종료 됐을 경우 리턴도 설정해주어야 한다*/
+			return 0;
+		}
+		
+		
+		/*public static int test(int num){
+			if (num >= 0) return 1;
+				}*/
+		}
---
+++ hello/src/day8/ex05.java
@@ -0,0 +1,36 @@
+package day8;
+
+public class ex05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			System.out.println(factorial(5));
+		}
+	
+	
+		// 잘못된 재귀 메소드 예제
+	public static void print(){
+		System.out.println("hello");
+		print();
+	}
+	
+	
+	
+	/*기능 - 정수 num이 주어지면 주어진 정수 num!를 알려주는 메소드
+	매개변수 - int num
+	리턴타입 - num!=>int
+	메서드명 - factorial*/  
+
+	public static int factorial(int num){
+		if (num == 1 || num == 0)
+			return 1;
+		if (num < 0)
+			return -1;
+		return factorial(num-1)*num;
+			
+	}
+
+}
+
