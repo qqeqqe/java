abba708e37aa0d29b8a1a87c0d9fb57b630693ff
younho <sino012ho@naver.com>
2019-04-24 17:01:49 +0900

    Project 계산기1

---
+++ hello/src/project1/Calculation.java
@@ -0,0 +1,50 @@
+package project1;
+
+import java.util.Scanner;
+
+public class Calculation {
+	
+	/*String op = "";
+	int Num1 = 0, Num2 = 0;
+	int sum = 0;
+
+	switch (op) {
+	case "+":
+		sum = Num1 + Num2;
+		System.out.println((sum));
+		break;
+
+	case "-":
+		sum = Num1 - Num2;
+		System.out.println((sum));
+		break;
+
+	case "*":
+		sum = Num1 * Num2;
+		System.out.println((sum));
+		break;
+
+	// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+	case "/":
+		if (Num2 == 0) {
+			System.out.println("0으로 나눌수 없습니다");
+		} else {
+			sum = Num1 / Num2;
+			System.out.println(sum);
+		}
+		break;
+
+	// ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+	case "%":
+		if (Num2 == 0) {
+			System.out.println("0으로는 계산할수 없습니다 ");
+		} else {
+			sum = Num1 % Num2;
+			System.out.println(sum);
+		}
+		break;
+
+	}// 스위치문의 끝
+*/
+	}
+
